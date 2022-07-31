a3ecf7e691f4875953aecf4ab54f8dd9a52c3c61
lipengfei <1401446942@qq.com>
2022-07-31 17:37:55 +0800

    style: add element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <router-link to="/login">login</router-link> |
   <router-link to="/main">main</router-link>
-
+  <el-button type="primary">123</el-button>
   <router-view />
 </template>
-
+<script lang="ts"></script>
 <style lang="less"></style>
