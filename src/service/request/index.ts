a537458c07b040ab72a86f612f069c1f8db598a0
lipengfei <1401446942@qq.com>
2022-08-03 22:47:18 +0800

    fix: axios封装loading

--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import type, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
-
+import { ElLoading } from 'element-plus'
 interface myRequestInterceptors {
   requestInterceptor?: (config: AxiosRequestConfig) => void
   requestInterceptorCatch?: (err: any) => any
@@ -14,6 +14,7 @@ interface IRequestConfig extends AxiosRequestConfig {
 export default class MyRequest {
   instance: AxiosInstance
   Interceptor?: myRequestInterceptors
+  Iloading: any
   constructor(config: IRequestConfig) {
     this.instance = axios.create(config)
     this.Interceptor = config.Interceptor
@@ -26,6 +27,19 @@ export default class MyRequest {
       this?.Interceptor?.responseInterceptor,
       this?.Interceptor?.responseInterceptorCatch
     )
+    //所有请求的拦截器
+    this.instance.interceptors.request.use((config) => {
+      this.Iloading = ElLoading.service({
+        lock: true,
+        text: 'Loading',
+        background: 'rgba(0, 0, 0, 0.7)'
+      })
+      return config
+    })
+    this.instance.interceptors.response.use((config) => {
+      this.Iloading.close()
+      return config
+    })
   }
   get(url: string, config?: AxiosResponse) {
     this.instance.get(url, config)
